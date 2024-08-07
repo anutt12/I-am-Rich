1d016cfd77c0d673930669df0e0794efa306521c
Andrew Nutt <87488155+anutt12@users.noreply.github.com>
2024-08-07 12:33:08 -0700

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Andrew Nutt on 8/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
